d032968c20c5ec7cd7b15b7bbef77cb5d6fbe96d
Sang Dang <sangdth@gmail.com>
2023-08-30 00:52:36 +0300

    removed domain too, we dont need it

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,31 @@
-import "@/styles/globals.css";
-// import { cal, inter } from "@/styles/fonts";
-import { Analytics } from "@vercel/analytics/react";
-import { Providers } from "./providers";
-import { Metadata } from "next";
-// import { cn } from "@/lib/utils";
+import '@/styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
+import { Providers } from './providers';
+import { Metadata } from 'next';
 
 const title =
-  "Platforms Starter Kit – The all-in-one starter kit for building multi-tenant applications.";
+  'Platforms Starter Kit – The all-in-one starter kit for building multi-tenant applications.';
 const description =
-  "The Platforms Starter Kit is a full-stack Next.js app with multi-tenancy and custom domain support. Built with Next.js App Router, Vercel Postgres and the Vercel Domains API.";
-const image = "https://vercel.pub/thumbnail.png";
+  'The Platforms Starter Kit is a full-stack Next.js app with multi-tenancy and custom domain support. Built with Next.js App Router, Vercel Postgres and the Vercel Domains API.';
+const image = 'https://vercel.pub/thumbnail.png';
 
 export const metadata: Metadata = {
   title,
   description,
-  icons: ["https://vercel.pub/favicon.ico"],
+  icons: ['https://vercel.pub/favicon.ico'],
   openGraph: {
     title,
     description,
     images: [image],
   },
   twitter: {
-    card: "summary_large_image",
+    card: 'summary_large_image',
     title,
     description,
     images: [image],
-    creator: "@vercel",
+    creator: '@vercel',
   },
-  metadataBase: new URL("https://vercel.pub"),
+  metadataBase: new URL('https://vercel.pub'),
 };
 
 export default function RootLayout({
