161d0d1e31f54a0d8dd46b416d7cfc88fd8b1598
Sang Dang <sangdth@gmail.com>
2023-08-30 11:50:55 +0300

    installed chakra

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,22 @@
-"use client";
+'use client';
 
-import { SessionProvider } from "next-auth/react";
-import { Toaster } from "sonner";
-import { ModalProvider } from "@/components/modal/provider";
+import { SessionProvider } from 'next-auth/react';
+import { CacheProvider } from '@chakra-ui/next-js';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Toaster } from 'sonner';
+import { ModalProvider } from '@/components/modal/provider';
+import { theme } from '@/lib/chakra';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <Toaster className="dark:hidden" />
-      <Toaster theme="dark" className="hidden dark:block" />
-      <ModalProvider>{children}</ModalProvider>
+      <CacheProvider>
+        <ChakraProvider theme={theme}>
+          <Toaster className="dark:hidden" />
+          <Toaster theme="dark" className="hidden dark:block" />
+          <ModalProvider>{children}</ModalProvider>
+        </ChakraProvider>
+      </CacheProvider>
     </SessionProvider>
   );
 }
---
+++ lib/chakra.ts
@@ -0,0 +1,79 @@
+import { extendTheme, UseToastOptions } from '@chakra-ui/react';
+
+export const toastOptions: UseToastOptions = {
+  position: 'top-right',
+  duration: 1000,
+  status: 'success',
+};
+
+const colorModeInLocalStorage =
+  typeof window !== 'undefined'
+    ? window.localStorage.getItem('chakra-ui-color-mode')
+    : 'system';
+
+const config = {
+  initialColorMode: 'system',
+  useSystemColorMode: colorModeInLocalStorage === 'system',
+};
+
+const colors = {
+  brand: {
+    50: '#F78DA7',
+    100: '#F57393',
+    200: '#F3597F',
+    300: '#F23F6C',
+    400: '#F02558',
+    500: '#E91046',
+    600: '#CF0E3F',
+    700: '#B50D37',
+    800: '#9B0B2F',
+    900: '#810927',
+  },
+  yellowAlpha: {
+    50: 'rgba(236, 201, 75, 0.04)',
+    100: 'rgba(236, 201, 75, 0.06)',
+    200: 'rgba(236, 201, 75, 0.08)',
+    300: 'rgba(236, 201, 75, 0.16)',
+    400: 'rgba(236, 201, 75, 0.24)',
+    500: 'rgba(236, 201, 75, 0.36)',
+    600: 'rgba(236, 201, 75, 0.48)',
+    700: 'rgba(236, 201, 75, 0.64)',
+    800: 'rgba(236, 201, 75, 0.80)',
+    900: 'rgba(236, 201, 75, 0.92)',
+  },
+};
+
+const styles = {
+  global: {
+    '.some-global-class': {
+      paddingY: 2,
+    },
+
+    a: {
+      // Fix issue links wrap outside of button have ugly sharp corners
+      borderRadius: 'md',
+    },
+  },
+};
+
+const components = {
+  Input: {
+    variants: {
+      expanding: () => ({
+        field: {
+          // Merge default styles
+          // ...mode('light')(props).field,
+          // ...mode('dark')(props).field,
+          background: 'transperant',
+          width: '50%',
+          transition: 'width 0.3s',
+          _focus: {
+            width: '100%',
+          },
+        },
+      }),
+    },
+  },
+};
+
+export const theme = extendTheme({ config, colors, components, styles });
