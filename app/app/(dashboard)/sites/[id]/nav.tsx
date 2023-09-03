2d2195e5a4a53ee219fd42b8b8cc45b54584e095
Sang Dang <sangdth@gmail.com>
2023-09-03 18:07:27 +0300

    refactor nav to chakra

--- app/app/(dashboard)/sites/[id]/nav.tsx
+++ app/app/(dashboard)/sites/[id]/nav.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { cn } from '@/lib/utils';
+import { Button, Stack } from '@/components';
 import Link from 'next/link';
 import { useParams, useSelectedLayoutSegment } from 'next/navigation';
 
@@ -26,23 +26,21 @@ export default function SiteSettingsNav() {
     },
   ];
 
+  const checkVariant = (item: (typeof navItems)[0]) => {
+    if (segment === item.segment) {
+      return 'solid';
+    }
+
+    return 'ghost';
+  };
+
   return (
-    <div className="flex space-x-4 border-b border-stone-200 pb-4 pt-2 dark:border-stone-700">
+    <Stack direction="row">
       {navItems.map((item) => (
-        <Link
-          key={item.name}
-          href={item.href}
-          // Change style depending on whether the link is active
-          className={cn(
-            'rounded-md px-2 py-1 text-sm font-medium transition-colors active:bg-stone-200 dark:active:bg-stone-600',
-            segment === item.segment
-              ? 'bg-stone-100 text-stone-600 dark:bg-stone-800 dark:text-stone-400'
-              : 'text-stone-600 hover:bg-stone-100 dark:text-stone-400 dark:hover:bg-stone-800'
-          )}
-        >
+        <Button as={Link} key={item.name} href={item.href} variant={checkVariant(item)}>
           {item.name}
-        </Link>
+        </Button>
       ))}
-    </div>
+    </Stack>
   );
 }
