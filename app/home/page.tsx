f6c1cafb770dbf2ac02490eefa8af1f0ab3f1ae2
Sang Dang <sangdth@gmail.com>
2023-09-05 12:55:36 +0300

    change home page url

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,15 @@
+import { IS_PRODUCTION, NEXT_PUBLIC_ROOT_DOMAIN } from '@/lib/constants';
 import { Button, Heading } from '@/components/chakra';
 import { NextLink } from '@/components/client';
 
 export default function HomePage() {
+  const url = `${IS_PRODUCTION ? 'https' : 'http'}://app.${NEXT_PUBLIC_ROOT_DOMAIN}`;
+
   return (
     <>
       <Heading>Edit this page on app/home/page.tsx</Heading>
 
-      <Button as={NextLink} href="http://app.localhost:3000">
+      <Button as={NextLink} href={url}>
         Start creating
       </Button>
     </>
