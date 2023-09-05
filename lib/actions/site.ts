9ed5aee4e713fe775b4eb279f477c3cac0a3ba47
Sang Dang <sangdth@gmail.com>
2023-09-05 12:46:05 +0300

    removed helpers unused

--- lib/actions/site.ts
+++ lib/actions/site.ts
@@ -1,26 +1,11 @@
 'use server';
 
-import {
-  addDomainToVercel,
-  removeDomainFromVercelProject,
-  validDomainRegex,
-} from '@/lib/domains';
 import { revalidateTag } from 'next/cache';
-import { customAlphabet } from 'nanoid';
-import { put } from '@vercel/blob';
 import crypto from 'crypto';
 
-import { getBlurDataURL } from '@/lib/utils';
 import { getSession } from '@/lib/auth';
 import { prisma } from '@/lib/prisma';
 import type { Site } from '@/types';
-
-import { withSiteAuth } from '../auth';
-
-const nanoid = customAlphabet(
-  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-  7
-); // 7-character random string
 
 export type CreateSiteDto = {
   name: string;
@@ -99,133 +84,6 @@ export const updateSiteSimple = async (
 
   return response;
 };
-
-export const updateSite = withSiteAuth(async (formData: any, site: Site, key: string) => {
-  const value = formData.get(key) as string;
-
-  try {
-    let response;
-
-    if (key === 'customDomain') {
-      if (value.includes('vercel.pub')) {
-        return {
-          error: 'Cannot use vercel.pub subdomain as your custom domain',
-        };
-
-        // if the custom domain is valid, we need to add it to Vercel
-      } else if (validDomainRegex.test(value)) {
-        response = await prisma.site.update({
-          where: {
-            id: site.id,
-          },
-          data: {
-            customDomain: value,
-          },
-        });
-        await addDomainToVercel(value);
-
-        // empty value means the user wants to remove the custom domain
-      } else if (value === '') {
-        response = await prisma.site.update({
-          where: {
-            id: site.id,
-          },
-          data: {
-            customDomain: null,
-          },
-        });
-      }
-
-      // if the site had a different customDomain before, we need to remove it from Vercel
-      if (site.customDomain && site.customDomain !== value) {
-        response = await removeDomainFromVercelProject(site.customDomain);
-
-        /* Optional: remove domain from Vercel team 
-
-          // first, we need to check if the apex domain is being used by other sites
-          const apexDomain = getApexDomain(`https://${site.customDomain}`);
-          const domainCount = await prisma.site.count({
-            where: {
-              OR: [
-                {
-                  customDomain: apexDomain,
-                },
-                {
-                  customDomain: {
-                    endsWith: `.${apexDomain}`,
-                  },
-                },
-              ],
-            },
-          });
-
-          // if the apex domain is being used by other sites
-          // we should only remove it from our Vercel project
-          if (domainCount >= 1) {
-            await removeDomainFromVercelProject(site.customDomain);
-          } else {
-            // this is the only site using this apex domain
-            // so we can remove it entirely from our Vercel team
-            await removeDomainFromVercelTeam(
-              site.customDomain
-            );
-          }
-          
-          */
-      }
-    } else if (key === 'image' || key === 'logo') {
-      if (!process.env.BLOB_READ_WRITE_TOKEN) {
-        return {
-          error:
-            'Missing BLOB_READ_WRITE_TOKEN token. Note: Vercel Blob is currently in beta – please fill out this form for access: https://tally.so/r/nPDMNd',
-        };
-      }
-
-      const file = formData.get(key) as File;
-      const filename = `${nanoid()}.${file.type.split('/')[1]}`;
-
-      const { url } = await put(filename, file, {
-        access: 'public',
-      });
-
-      const blurhash = key === 'image' ? await getBlurDataURL(url) : null;
-
-      response = await prisma.site.update({
-        where: {
-          id: site.id,
-        },
-        data: {
-          [key]: url,
-          ...(blurhash && { imageBlurhash: blurhash }),
-        },
-      });
-    } else {
-      response = await prisma.site.update({
-        where: {
-          id: site.id,
-        },
-        data: {
-          [key]: value,
-        },
-      });
-    }
-
-    revalidateTag(`${site.subdomain}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}-metadata`);
-    site.customDomain && revalidateTag(`${site.customDomain}-metadata`);
-
-    return response;
-  } catch (error: any) {
-    if (error.code === 'P2002') {
-      return {
-        error: `This ${key} is already taken`,
-      };
-    } else {
-      return {
-        error: error.message,
-      };
-    }
-  }
-});
 
 export const deleteSite = async (site: Site) => {
   try {
